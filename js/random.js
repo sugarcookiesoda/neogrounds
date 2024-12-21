eb286c102aa8e5feb0bb0d435458a3f28a6c30b6
sugarcookiesoda <123298169+sugarcookiesoda@users.noreply.github.com>
2024-12-21 13:07:20 -0800

    Add files via upload

---
+++ js/random.js
@@ -0,0 +1,8 @@
+var sentences = new Array(
+
+    "these used to be cringe",
+    "hi there",
+    "check out an actual good project @ <a href='https://the-duck.co'></a>",
+    "quack quack",
+    "does anyone actually use this still"
+    );
